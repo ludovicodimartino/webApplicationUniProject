66c51dbdb383f111d976784152317a7cd337bd39
Michele Scapinello <michele.scapinello@studenti.unipd.it>
2024-04-12 12:45:08 +0200

    Car List and Circuit List

---
+++ src/main/java/it/unipd/dei/webapp/wacar/dao/ListCircuitDAO.java
@@ -0,0 +1,68 @@
+package it.unipd.dei.webapp.wacar.dao;
+
+import it.unipd.dei.webapp.wacar.resource.Circuit;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is the data access object for listing the circuits in the database
+ *
+ * @author Michele Scapinello (michele.scapinello@studenti.unipd.it)
+ * @version 1.00
+ * @since 1.00
+ */
+public class ListCircuitDAO extends AbstractDAO<List<Circuit>> {
+    private static final String STATEMENT = "Select * from assessment.circuit";
+
+    /**
+     * Creates a new DAO object.
+     *
+     * @param con the connection to be used for accessing the database.
+     */
+    public ListCircuitDAO(Connection con) {
+        super(con);
+    }
+
+    /**
+     * Performs the access to the data source by executing the query and returning the results
+     */
+    @Override
+    protected void doAccess() throws Exception {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        final List<Circuit> circuits = new ArrayList<>();
+
+        try {
+            pstmt = con.prepareStatement(STATEMENT);
+
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                circuits.add(new Circuit(
+                        rs.getString("name"),
+                        rs.getString("type"),
+                        rs.getInt("length"),
+                        rs.getInt("cornersNumber"),
+                        rs.getString("address"),
+                        rs.getString("description"),
+                        rs.getInt("lapPrice"),
+                        rs.getBoolean("available"),
+                        rs.getString("image")));
+            }
+
+            LOGGER.info("All circuits successfully retrieved");
+
+        } finally {
+            if (rs != null)
+                rs.close();
+            if (pstmt != null)
+                pstmt.close();
+        }
+        this.outputParam = circuits;
+    }
+}
---
+++ src/main/java/it/unipd/dei/webapp/wacar/servlet/CircuitListServlet.java
@@ -0,0 +1,55 @@
+package it.unipd.dei.webapp.wacar.servlet;
+
+import it.unipd.dei.webapp.wacar.dao.ListCircuitDAO;
+import it.unipd.dei.webapp.wacar.resource.Circuit;
+import it.unipd.dei.webapp.wacar.resource.Message;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "CircuitListServlet", value = "/circuit_list")
+public class CircuitListServlet extends AbstractDatabaseServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        req.getSession().invalidate();
+
+        LogContext.setIPAddress(req.getRemoteAddr());
+        LogContext.setResource(req.getRequestURI());
+        LogContext.setAction("GET_ALL_CIRCUITS_FROM_DATABASE");
+
+        List<Circuit> circuits = null;
+        Message m = null;
+
+        try {
+            circuits = new ListCircuitDAO(getConnection()).access().getOutputParam();
+
+            m = new Message("Circuits successfully retrieved");
+
+            LOGGER.info("Circuits successfully retrieved");
+
+        } catch (SQLException ex) {
+            m = new Message("Cannot search for circuits: unexpected error while accessing the database.", "E200", ex.getMessage());
+
+            LOGGER.error("Cannot search for circuits: unexpected error while accessing the database.", ex);
+        } finally {
+            LogContext.removeIPAddress();
+            LogContext.removeAction();
+            LogContext.removeUser();
+        }
+
+        // Set the list of cars and the message as request attributes
+        req.setAttribute("circuits", circuits);
+        req.setAttribute("message", m);
+
+        // Forward the request to the JSP page
+        req.getRequestDispatcher("/jsp/circuit-list.jsp").forward(req, res);
+    }
+}
