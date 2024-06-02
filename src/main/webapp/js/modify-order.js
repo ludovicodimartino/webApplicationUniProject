6204756287fe5d2b810585b82c0b494693fc924a
FilippoG2001 <filippo.galli@studenti.unipd.it>
2024-06-02 20:38:22 +0200

    fix modify-order

--- src/main/webapp/js/modify-order.js
+++ src/main/webapp/js/modify-order.js
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('[id^="orderDate"]').forEach(function(orderDate) {
         const orderId = orderDate.id.replace('orderDate', '');
-        const confirmButton = document.getElementById("confirmButton" + orderId);
 
         orderDate.addEventListener("input", function() {
             const selectedDate = new Date(orderDate.value);
@@ -34,12 +33,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
             if (dayDifference < 2) {
                 orderDate.classList.add("is-invalid");
-                confirmButton.disabled = true;
             } else {
                 orderDate.classList.remove("is-invalid");
-                if (!document.getElementById("nLaps" + orderId).classList.contains("is-invalid")) {
-                    confirmButton.disabled = false;
-                }
             }
         });
     });
@@ -48,19 +43,14 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('[id^="nLaps"]').forEach(function(orderNLaps) {
         const orderId = orderNLaps.id.replace('nLaps', '');
-        const confirmButton = document.getElementById("confirmButton" + orderId);
 
         orderNLaps.addEventListener("input", function() {
             const nLaps = parseInt(orderNLaps.value);
 
             if (nLaps <= 0 || isNaN(nLaps)) {
                 orderNLaps.classList.add("is-invalid");
-                confirmButton.disabled = true;
             } else {
                 orderNLaps.classList.remove("is-invalid");
-                if (!document.getElementById("orderDate" + orderId).classList.contains("is-invalid")) {
-                    confirmButton.disabled = false;
-                }
             }
         });
     });
